56568b707dc6a9e5aad4db42593d0cda3cbbb56b
Haseeb30000 <34556583+Haseeb30000@users.noreply.github.com>
2022-10-20 00:14:30 +0500

    Updates

---
+++ precache-manifest.dfa2572f0c30c069161d381a7dc65f2b.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bf65b97bd319646c5b50a024d0a65e",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/index.html"
+  },
+  {
+    "revision": "a826e6a494250a447f34",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/css/9.5ce1bf39.chunk.css"
+  },
+  {
+    "revision": "9261fca4fa206370baab",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/css/main.4cbb4487.chunk.css"
+  },
+  {
+    "revision": "5b12428a7333adc9d530",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/0.6945cf60.chunk.js"
+  },
+  {
+    "revision": "c360eff85ca112d098c080b19dff9b78",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/0.6945cf60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ae7380ecf05ff03139",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/1.e53867dc.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/1.e53867dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0c56cf65eb6e07f430",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/10.11b90f09.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/10.11b90f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d189d0d912d12fd74e4",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/11.e5f30e57.chunk.js"
+  },
+  {
+    "revision": "9edac4164194611b6ee8",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/12.afc0ec9b.chunk.js"
+  },
+  {
+    "revision": "a62180261fd0d06d3655",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/13.e0a8eb91.chunk.js"
+  },
+  {
+    "revision": "15928c8139c489ab93bf",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/14.042d6f2e.chunk.js"
+  },
+  {
+    "revision": "a8c04632311e36d5a487",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/15.bc3fe83c.chunk.js"
+  },
+  {
+    "revision": "1df2ee8028b1cd815a7f",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/16.374d5715.chunk.js"
+  },
+  {
+    "revision": "4104320e4c1ac6982caf",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/17.83bd481f.chunk.js"
+  },
+  {
+    "revision": "02e0af3b1accd40d825c",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/18.e53ba05d.chunk.js"
+  },
+  {
+    "revision": "418965da249c4f6817ab",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/19.4ab3f4af.chunk.js"
+  },
+  {
+    "revision": "080f95199389e65b8d00",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/2.817ff108.chunk.js"
+  },
+  {
+    "revision": "91fd3a51eb11ff2c2044",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/20.b49d4c6d.chunk.js"
+  },
+  {
+    "revision": "ffb9360da4a8e445abae",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/21.599c6c6c.chunk.js"
+  },
+  {
+    "revision": "161504bb0b09645f38ed",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/22.fe8607eb.chunk.js"
+  },
+  {
+    "revision": "618ed0f70bc5cfde3c23",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/3.0c74166b.chunk.js"
+  },
+  {
+    "revision": "408de60a9dcd5f18c214",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/6.84c4bb1c.chunk.js"
+  },
+  {
+    "revision": "458266476ee85d23c06653544e60ce99",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/6.84c4bb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828d4405536a39fe3ac3",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/7.13f90a99.chunk.js"
+  },
+  {
+    "revision": "a64acf8e3ad6aa1497056f0bb9a7ce4f",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/7.13f90a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0bcbbb1fac9eccacc7",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/8.da796539.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/8.da796539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a826e6a494250a447f34",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/9.21290e31.chunk.js"
+  },
+  {
+    "revision": "9261fca4fa206370baab",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/main.3a31321e.chunk.js"
+  },
+  {
+    "revision": "0434724ff2842015131b",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/js/runtime-main.e49c0347.js"
+  },
+  {
+    "revision": "b9c73089fd14dcb97bf597665f575ac1",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/bell.b9c73089.svg"
+  },
+  {
+    "revision": "ae6d8356288db42a438a49fc4bd53b4e",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/buttons.ae6d8356.svg"
+  },
+  {
+    "revision": "be9df68bf5080eca68b65f6718296fae",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/cards.be9df68b.svg"
+  },
+  {
+    "revision": "977becb6db37f454bad0a5b18f6e647a",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/cart.977becb6.svg"
+  },
+  {
+    "revision": "057753b09a212e5160fe854595ffe79f",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/charts.057753b0.svg"
+  },
+  {
+    "revision": "f7fdff0357cd6bbf4d41ff8aac44a033",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/chat.f7fdff03.svg"
+  },
+  {
+    "revision": "c34040acd2b1249c7cd75ce72a0a36f6",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/dropdown.c34040ac.svg"
+  },
+  {
+    "revision": "a4cc801985d0368571b7e31dfaf62769",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/edit.a4cc8019.svg"
+  },
+  {
+    "revision": "df8aaef727cafdb462dc6a3ef66021d6",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/forbidden.df8aaef7.svg"
+  },
+  {
+    "revision": "37ff675a985578056d0f380370db2e91",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/forms.37ff675a.svg"
+  },
+  {
+    "revision": "d2784fb58776d9620333fe1ca74cea95",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/gift.d2784fb5.png"
+  },
+  {
+    "revision": "77016819da953ea8a4a60950acb99b99",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/github.77016819.svg"
+  },
+  {
+    "revision": "0d8cfa09614809887b157eba25082af1",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/heart.0d8cfa09.svg"
+  },
+  {
+    "revision": "0717747219593ac12c6f355006f97943",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/home.07177472.svg"
+  },
+  {
+    "revision": "e8edbe5b89f1a31eb88a2ff2d3cb34eb",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/mail.e8edbe5b.svg"
+  },
+  {
+    "revision": "df9ba7c81d8c805ebf3d84a48bd9d57c",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/menu.df9ba7c8.svg"
+  },
+  {
+    "revision": "62a5a8fff7ff6bef7bf961f2a889dc21",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/modals.62a5a8ff.svg"
+  },
+  {
+    "revision": "f88a7a4e766a011150d1f19f3828bb51",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/money.f88a7a4e.svg"
+  },
+  {
+    "revision": "cdf19a70cc4aad218e3b50d3ee1f6866",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/moon.cdf19a70.svg"
+  },
+  {
+    "revision": "570b0d66f31d545fdbdc30f5eefaa126",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/outlineCog.570b0d66.svg"
+  },
+  {
+    "revision": "42f24449f7bd9928f5ea1d0fd07d95e7",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/outlineLogout.42f24449.svg"
+  },
+  {
+    "revision": "50f0ae3e598fe90d94336b5492c772c1",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/outlinePerson.50f0ae3e.svg"
+  },
+  {
+    "revision": "6650d5109fafc0724eac274c3b43eed5",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/pages.6650d510.svg"
+  },
+  {
+    "revision": "c4fada77c844618fef90d2194039d452",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/people.c4fada77.svg"
+  },
+  {
+    "revision": "0fe47b33e2d6c6a606d5c0d31af8242a",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/scrt_logo.0fe47b33.png"
+  },
+  {
+    "revision": "70205477154d7db80b2f05e9716eb3c1",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/search.70205477.svg"
+  },
+  {
+    "revision": "e72f14b8a27c721f10d5eb87e8ec3d2f",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/sun.e72f14b8.svg"
+  },
+  {
+    "revision": "b9ce44e708d154ec60a8deabeb3e8e58",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/tables.b9ce44e7.svg"
+  },
+  {
+    "revision": "f3c7c3b232acfe1a6250f640a0a17741",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/trash.f3c7c3b2.svg"
+  },
+  {
+    "revision": "c6bd77d7d4321627a185c1cbc89276d8",
+    "url": "/securesecrets/GalacticPoolsTestnetFrontend/static/media/twitter.c6bd77d7.svg"
+  }
+]);
